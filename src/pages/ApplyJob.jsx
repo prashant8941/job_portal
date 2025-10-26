c898d485b56395481c9c895e73dbcebf24313022
prashant8941 <ps65854321@gmail.com>
2025-10-26 20:53:03 +0530

    adding loading.jsx file for the loading animation

--- src/pages/ApplyJob.jsx
+++ src/pages/ApplyJob.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import { useState, useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+import { set } from 'mongoose';
+import Loading from '../components/Loading';
 
 const ApplyJob = () => {
-  return (
+  const{id} = useParams();
+  const[jobData, setJobData] = useState(null);
+
+  const{jobs} = useContext(AppContext);
+
+  const fetchJob = async() =>{
+    const data = jobs.filter(job=>job._id=== id)
+    if(data.length !==  0 ){
+      setJobData(data[0]);
+      console.log(data[0]);
+    }
+  }
+
+  useEffect(() => {
+    if(jobs.length > 0 ){
+          fetchJob();
+
+    }
+  },[id,jobs]);
+  
+
+  return  jobData?  (
     <div>
       
     </div>
+  ):(
+<Loading/>
   )
 }
 
