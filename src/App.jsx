6a1444f9de5a846e66028fbfc3d8f7e883e9ea69
prashant8941 <ps65854321@gmail.com>
2025-10-27 23:58:15 +0530

    adding the data for the jobs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import ApplyJob from './pages/ApplyJob.jsx'
 import Applications from './pages/Applications.jsx'   
-import Navbar from './components/Navbar.jsx'
+
+
 
 const App = () => {
   return (
@@ -11,8 +12,8 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/apply-job/:id" element={<div>ApplyJob</div>} />
-        <Route path="/applications" element={<div>Applications</div>} />
+        <Route path="/apply-job/:id" element={<ApplyJob/>} />
+        <Route path="/applications" element={<Applications />} />
       </Routes>
       
       
